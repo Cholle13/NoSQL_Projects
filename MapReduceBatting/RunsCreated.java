ffa250aed56bead3fa99527210dbb781384925b4
Chris Holle <Christopher_Holle@baylor.edu>
2020-01-30 16:28:13 -0600

    Update code for output file name change

--- MapReduceBatting/RunsCreated.java
+++ MapReduceBatting/RunsCreated.java
@@ -100,6 +100,7 @@ public class RunsCreated {
     job.setReducerClass(IntSumReducer.class);
     job.setOutputKeyClass(Text.class);
     job.setOutputValueClass(DoubleWritable.class);
+    job.getConfiguration().set("mapreduce.output.basename", "RunsCreated_out");
     FileInputFormat.addInputPath(job, new Path(args[0]));
     FileOutputFormat.setOutputPath(job, new Path(args[1]));
     System.exit(job.waitForCompletion(true) ? 0 : 1);
