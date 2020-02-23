10c5ad011643dda57752945a65c78a82156be90b
Chris Holle <Christopher_Holle@baylor.edu>
2020-02-23 17:30:09 -0600

    Job Configurations Team/Player joins

--- MapReduceComplexJoin/ComplexJoin.java
+++ MapReduceComplexJoin/ComplexJoin.java
@@ -8,8 +8,13 @@
  * by each player in Batting.csv over their career.
  */
 import java.io.IOException;
+import java.io.FileNotFoundException;
 import java.util.StringTokenizer;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.net.URI;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
@@ -23,6 +28,7 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.filecache.DistributedCache;
 
 public class ComplexJoin {
   // Mapper
@@ -64,6 +70,7 @@ public class ComplexJoin {
 	public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
 		String line = value.toString();
 		String[] attribute_str = line.split(",");
+		if (!attribute_str[0].equals("")) {
 
 		if(!attribute_str[0].equals("playerID")) {
 			String player_id = attribute_str[0];
@@ -79,50 +86,80 @@ public class ComplexJoin {
 			dataStringBuilder.append(attribute_str[11].toString());
 			String dataString = dataStringBuilder.toString();
 			dataStringBuilder = null;
+			if (!attribute_str.equals("")) {
 			if (Integer.parseInt(attribute_str[1]) > 1899) {
 				context.write(new Text(player_id), new Text(dataString));
 			}
+			}
+		}
 		}
 
 	}
   }
   public static class TeamDataMapper extends Mapper<Object, Text, Text, Text> {
-	final static int NUM_TEAM_ATTRIBUTES = 20;
+	// Stores HashMap of Franchise ID to Franchise Name 
+	private static HashMap<String, String> TeamFranchMap = new HashMap<String, String>();
+	// Used to read from DCache
+	private BufferedReader reader;
+	private String franchName = "";
+	private Text txtOutputKey = new Text("");
+	private Text txtOutputValue = new Text("");
+
+	enum FLAG { RECORD_COUNT, FILE_EXISTS, FILE_NOT_FOUND, ERROR }
+
+	// Setup the Local Cache to find the correct File.
+	@Override
+	protected void setup(Context context) throws IOException, InterruptedException {
+		Path[] cacheLocal = context.getLocalCacheFiles(); 
+		for (Path path : cacheLocal) {
+			if (path.getName().toString().trim().equalsIgnoreCase("TeamsFranchises.csv")) {
+				context.getCounter(FLAG.FILE_EXISTS).increment(1);
+				loadFranchHashMap(path, context);
+			}
+		}
+	}
+
+	// Setup the Fanchise name HashMap to store map of Franchise id to Franchise name
+	private void loadFranchHashMap(Path filePath, Context context) {
+		String line = "";
+		try {
+			reader = new BufferedReader(new FileReader(filePath.toString()));
+
+			// Read lines and load into HashMap
+			while ((line = reader.readLine()) != null) {
+				String franchAttr[] = line.split(",");
+				TeamFranchMap.put(franchAttr[0].trim(), franchAttr[1].trim());
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
+	}
+
 	public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
 		String line = value.toString();
-		String[] attribute_str = line.split(",");
-		
-		if(!attribute_str[0].equals("yearID")) {
-			String franch_id = attribute_str[3];
-			StringBuilder dataStringBuilder = new StringBuilder();
-			dataStringBuilder.append("Team-");
-			dataStringBuilder.append(attribute_str[0] + ",").append(attribute_str[19]);
-			if (Integer.parseInt(attribute_str[0]) > 1899) {
-				context.write(new Text(franch_id), new Text(dataStringBuilder.toString()));
+		context.getCounter(FLAG.RECORD_COUNT).increment(1);
+		if (line.length() > 0) {
+			String[] attribute_str = line.split(",");
+			if(!attribute_str[0].equals("yearID")) {
+				// Set the Franch id
+				String franch_id = attribute_str[3];
+				// Grab the franchise Name from the Franchise HashMap
+				franchName = TeamFranchMap.get(franch_id);
+				StringBuilder dataStringBuilder = new StringBuilder();
+				dataStringBuilder.append(attribute_str[19]);
+				if (Integer.parseInt(attribute_str[0]) > 1899 && (!franchName.equals("") || !franchName.equals(null))) {
+					// write the franchise name and year as key and the year, homerun and franchise name as the value
+					context.write(new Text(franchName + "," + attribute_str[0]), new Text(dataStringBuilder.toString()));
+				}
+				dataStringBuilder = null;
 			}
-			dataStringBuilder = null;
 		}
+		franchName = "";
 	}
   }	
 
-  public static class FranchDataMapper extends Mapper<Object, Text, Text, Text> {
-	  public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-		  String line = value.toString();
-		  String[] attribute_str = line.split(",");
 
-		  if(!attribute_str[0].equals("franchID")) {
-			  String franch_id = attribute_str[0];
-			  StringBuilder dataStringBuilder = new StringBuilder();
-			  dataStringBuilder.append("Franch-");
-			  dataStringBuilder.append(attribute_str[1]);
-			  context.write(new Text(franch_id), new Text(dataStringBuilder.toString()));
-			  dataStringBuilder = null;
-		  }
-	  }
-  }
-
-
-  // Reducer Output to be used in another job
+  // Player Reducer Output to be used in another job
   public static class PlayerReducer
        extends Reducer<Text,Text,Text,Text> {
     // Reducer function
@@ -159,6 +196,9 @@ public class ComplexJoin {
 	    		if (splitAttr.length > 3) {
 				yearID = splitAttr[0];
 				hr = splitAttr[3];
+				if (hr.equals("")) {
+					hr = "0";
+				}
 				// Remove this to remove year from key
 				keyBuilder.append("," + yearID);
 	    		}
@@ -204,30 +244,53 @@ public class ComplexJoin {
 	  }
   }
 
+  // Get Job Info
+  public static Job getPlayerJob(Configuration conf, String personInputPath, String playerInputPath, String outputPath) throws IOException {
+	  Job job = Job.getInstance(conf, "complex join");
+	  job.setJarByClass(ComplexJoin.class);
+	  job.setMapperClass(PlayerMapper.class);
+	  //job.setCombinerClass(PlayerReducer.class);
+	  job.setReducerClass(PlayerReducer.class);
+	  job.setOutputKeyClass(Text.class);
+	  job.setOutputValueClass(Text.class);
+	  MultipleInputs.addInputPath(job, new Path(personInputPath), TextInputFormat.class, PlayerMapper.class);
+	  MultipleInputs.addInputPath(job, new Path(playerInputPath), TextInputFormat.class, PlayerDataMapper.class);
+	  FileOutputFormat.setOutputPath(job, new Path(outputPath));
+	  job.getConfiguration().set("mapreduce.output.basename", "Player_out");
+	  return job;
+  }
+  // Get Team job info
+  public static Job getTeamJob(Configuration conf, String inputPath, String cachePath, String outputPath) throws IOException {
+	  Job job = Job.getInstance(conf, "complex join");
+	  job.setJarByClass(ComplexJoin.class);
+	  job.setMapperClass(TeamDataMapper.class);
+	  job.setNumReduceTasks(0);
+	  job.addCacheFile(new Path(cachePath).toUri());
+	  FileInputFormat.setInputPaths(job, new Path(inputPath));
+	  FileOutputFormat.setOutputPath(job, new Path(outputPath));
+	  job.setOutputKeyClass(Text.class);
+	  job.setOutputValueClass(Text.class);
+	  job.getConfiguration().set("mapreduce.output.basename", "Team_out");
+	  return job;
+  }
+
+
   public static void main(String[] args) throws Exception {
     // Configuration stuff
     Configuration conf = new Configuration();
-    Job job = Job.getInstance(conf, "complex join");
-    job.setJarByClass(ComplexJoin.class);
-    // Player job
-    job.setMapperClass(PlayerMapper.class);
-    //job.setCombinerClass(PlayerReducer.class);
-    job.setReducerClass(PlayerReducer.class);
-    // Team Job
-    //job.setMapperClass(TeamDataMapper.class);
-    //job.setCombinerClass(TeamReducer.class);                                                                                                                                                                                          
-    //job.setReducerClass(TeamReducer.class);
-    job.setOutputKeyClass(Text.class);
-    job.setOutputValueClass(Text.class);
-    // Player Job
-    MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, PlayerMapper.class);
-    MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, PlayerDataMapper.class);
-    // Team job
-    //MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, TeamDataMapper.class);
-    //MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, FranchDataMapper.class);
-    job.getConfiguration().set("mapreduce.output.basename", "Complex_out");
-    FileOutputFormat.setOutputPath(job, new Path(args[2]));
-    System.exit(job.waitForCompletion(true) ? 0 : 1);
+    String playerInfoInput = args[0];
+    String playerStatsInput = args[1]; 
+    String playerOutput = args[2];
+    String teamStatsInput = args[3];
+    String teamFranchInput = args[4];
+    String teamOutput = args[5];
+    String finalOutput = "/users/holle/";
+    Job playerJob = getPlayerJob(conf, playerInfoInput, playerStatsInput, playerOutput);
+    Job teamJob = getTeamJob(conf, teamStatsInput, teamFranchInput, teamOutput);
+
+    playerJob.waitForCompletion(true);
+
+    System.exit(teamJob.waitForCompletion(true) ? 0 : 1);
   }
 }
 /*
